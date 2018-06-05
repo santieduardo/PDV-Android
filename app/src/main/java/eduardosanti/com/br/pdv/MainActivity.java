36fa7793aedb928099eb7f7e7a5fa23f5b383297
Eduardo Santi <eduardo.santi@appfarm.com.br>
2018-06-04 21:26:07 -0300

    Validando e autenticando usuário

--- app/src/main/java/eduardosanti/com/br/pdv/MainActivity.java
+++ app/src/main/java/eduardosanti/com/br/pdv/MainActivity.java
@@ -1,13 +1,87 @@
 package eduardosanti.com.br.pdv;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import eduardosanti.com.br.pdv.model.User;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+
+    private ArrayList<User> users = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setUpUsers();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        this.clearFields();
+        this.emailEditText.requestFocus();
+    }
+
+    private void setUpUsers() {
+        if (this.users != null) {
+            this.users.add(new User("admin@gmail.com", "admin"));
+        }
+    }
+
+    public void loginPressed(View view) {
+        this.emailEditText = (EditText) findViewById(R.id.emailEditTextId);
+        this.passwordEditText = (EditText) findViewById(R.id.passwordEditTextId);
+
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if (!email.isEmpty() && !password.isEmpty()) {
+            User user = new User(email, password);
+
+            this.tryLogin(user);
+        } else {
+            this.displayToast("Preencha todos os campos");
+        }
+    }
+
+    private void tryLogin(User user) {
+        if (this.users.contains(user)) {
+            //this.goToWelcomeActivity(user);
+        } else {
+            this.displayToast("Usuário não encontrado");
+        }
+
+        this.dismissKeyboard();
+    }
+
+    private void clearFields() {
+        this.emailEditText.setText("");
+        this.passwordEditText.setText("");
+    }
+
+    private void displayToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void dismissKeyboard() {
+        View view = this.getCurrentFocus();
+
+        if (view != null) {
+            InputMethodManager inputManager = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 }
---
+++ app/src/main/java/eduardosanti/com/br/pdv/model/User.java
@@ -0,0 +1,40 @@
+package eduardosanti.com.br.pdv.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+}
