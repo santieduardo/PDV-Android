9b347a4d6cc4ee6a7ce57f551cc874cf03fd2635
Eduardo Santi <eduardo.santi@appfarm.com.br>
2018-06-09 02:20:53 -0300

    Criado CreateSellActivity

---
+++ app/src/main/java/eduardosanti/com/br/pdv/CreateSellActivity.java
@@ -0,0 +1,13 @@
+package eduardosanti.com.br.pdv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateSellActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_sell);
+    }
+}
