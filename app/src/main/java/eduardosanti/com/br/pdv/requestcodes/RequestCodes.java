be2824f14fb3bb354074a7effb2aa71d02754d47
Eduardo Santi <eduardo.santi@appfarm.com.br>
2018-06-08 23:07:24 -0300

    Adicionado RequestCodes para mapeamento de ResultActivities

---
+++ app/src/main/java/eduardosanti/com/br/pdv/requestcodes/RequestCodes.java
@@ -0,0 +1,8 @@
+package eduardosanti.com.br.pdv.requestcodes;
+
+public class RequestCodes {
+    public static final int DETAIL_SELL = 100;
+    public static final int CREATE_SELL = 101;
+    public static final int CHOOSE_PRODUCT = 102;
+}
+
