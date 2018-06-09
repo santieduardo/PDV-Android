35e9572dc60e19a11f4e249aaffe8796816ae496
Eduardo Santi <eduardo.santi@appfarm.com.br>
2018-06-08 23:10:43 -0300

    Criada DetailSellActivity

---
+++ app/src/main/java/eduardosanti/com/br/pdv/DetailSellActivity.java
@@ -0,0 +1,13 @@
+package eduardosanti.com.br.pdv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailSellActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_sell);
+    }
+}
