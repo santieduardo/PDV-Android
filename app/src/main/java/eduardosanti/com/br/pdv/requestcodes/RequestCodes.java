5e35515ceffb591358fa551c0167dfc8ecc81283
Eduardo Santi <eduardo.santi@appfarm.com.br>
2018-06-09 02:17:27 -0300

    Removendo RequestCode CHOOSE_PRODUCT

--- app/src/main/java/eduardosanti/com/br/pdv/requestcodes/RequestCodes.java
+++ app/src/main/java/eduardosanti/com/br/pdv/requestcodes/RequestCodes.java
@@ -3,6 +3,5 @@ package eduardosanti.com.br.pdv.requestcodes;
 public class RequestCodes {
     public static final int DETAIL_SELL = 100;
     public static final int CREATE_SELL = 101;
-    public static final int CHOOSE_PRODUCT = 102;
 }
 
